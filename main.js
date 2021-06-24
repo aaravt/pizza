402fe269081a44a6cd23af876344fd52f6a970b5
aaravt <79640699+aaravt@users.noreply.github.com>
2021-06-24 11:59:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,39 @@
-menu_list_array = ["Veg Margherita Pizza",
-                  "Chicken tandoori Pizza",
-                  "Veg supreme Pizza",
-                  "Paneer tikka Pizza",
-                  "Deluxe veggie Pizza",
-                  "Veg extravaganza Pizza"
-                    ];
+menu_list_array = [
+            " Veg Margherita Pizza",
+                  " Chicken tandoori Pizza",
+                  " Veg supreme Pizza",
+                  " Paneer tikka Pizza",
+                  " Deluxe veggie Pizza",
+                  " Veg extravaganza Pizza"
+                   ]
 
 function getmenu(){
-var htmldata;
-htmldata="<ol class='menulist'>"
-menu_list_array.sort();
-for (var i=0;menu_list_array.length[i];i++){
-    htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>' 
-
+var htmldata
+htmldata="ol class='menulist'>"
+menu_list_array_sort()
+for(var i=0;i<menu_list_array.length;i++)
+htmldata=htmldata<li> +menu_list_array[i] + '</li>'
 }
 htmldata=htmldata+"</ol>"
-document.getElementById("display_menu").innerHTML=htmldata
-function add_item(){
-var htmldata;
-var item=document.getElementById("add_item").value;
-menu_list_array.sort();
-htmldata="<section class='cards'>"
+document.getElementById("display_menu").innerHTML = htmldata
+
+
+
+function additem(){
+var htmldata
+var item=document.getElementById("add_item").value
+menu_list_array.push(item)
+menu_list_array.sort()
+htmldata="<section class =cards'>"
 for(var i=0;i<menu_list_array.length;i++){
-htmldata=htmldata+'<div class="card">'
-    +'<img src="images/pizzaImg.png"/>'
-        +menu_list_array[i] + '</div>'
+    htmldata=htmldata+'<div class ="card">' + '<img src = "images/pizzaimg.png"/>' + menu_list_array[i]
 }
-htmldata=htmldata="</section>"
-document.getElementById("display_added_menu").innerHTML=htmldata;
+htmldata=htmldata+"</section>"
+document.getElementById("display_addedmenu").innerHTML =htmldata;
+}
+
 function add_top(){
- var item=document.getElementById("add_item").value;
- menu_list_array.push(item);
- add_item();
-    }
+item=document.getElementById("add_item").value
+menu_list_array.push(item)
+additem()
+}
